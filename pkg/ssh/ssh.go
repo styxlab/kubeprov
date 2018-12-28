eafcb0c2ab035d5272dab0e68fc7517ef02e77bb
cws <cws@home.fritz.box>
2018-12-28 20:16:05 +0100

    latest

--- pkg/ssh/ssh.go
+++ pkg/ssh/ssh.go
@@ -57,7 +57,7 @@ func (key *Key) Config(user string) *Config {
 		User:            user,
 		Auth:            []ssh.AuthMethod{ssh.PublicKeys(key.signer)},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		Timeout:         60 * time.Second,
+		Timeout:         0 * time.Second,
 	}
 
 	return &Config{
