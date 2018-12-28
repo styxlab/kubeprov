a6035e2f30e36f3f5d7a9e0cd835b8cf00127520
cws <cws@home.fritz.box>
2018-12-28 23:20:25 +0100

    testing

--- pkg/ssh/shell.go
+++ pkg/ssh/shell.go
@@ -9,10 +9,10 @@ import (
 //ExecCmd executes directly via shell command
 func ExecCmd(user string, port string, ipAddress string, cmd string) error {
 
-	sshCommand := exec.Command("ssh", "-l", user, "-p", port, ipAddress, cmd)
-	//sshCommand.Stdin = os.Stdin
-	//sshCommand.Stdout = os.Stdout
-	//sshCommand.Stderr = os.Stderr
+	sshCommand := exec.Command("ssh", "-oStrictHostKeyChecking=no", "-l", user, "-p", port, ipAddress, cmd)
+	sshCommand.Stdin = os.Stdin
+	sshCommand.Stdout = os.Stdout
+	sshCommand.Stderr = os.Stderr
 
 	if err := sshCommand.Run(); err != nil {
 		if exitError, ok := err.(*exec.ExitError); ok {
