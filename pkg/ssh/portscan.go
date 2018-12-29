c099750b3a3ef66d1f2452287c25aa92619404c4
cws <cws@home.fritz.box>
2018-12-29 01:46:23 +0100

    testing

--- pkg/ssh/portscan.go
+++ pkg/ssh/portscan.go
@@ -9,28 +9,23 @@ import (
 )
 
 //Scans for open port
-func ScanPort(ip string, port int, timeout time.Duration) bool {
-
-    interval := 2 * time.Seconds
+func ScanPort(ip string, port int, interval time.Duration, timeout time.Duration) bool {
 
     if timeout < interval {
         return false
     }
-    
+
     target := fmt.Sprintf("%s:%d", ip, port)
     
     conn, err := net.DialTimeout("tcp", target, interval)
     
     if err != nil {
-        if strings.Contains(err.Error(), "connection refused") {
-            time.Sleep(interval)
-            ScanPort(ip, port, timeout - intervall)
-        } else {
-            fmt.Println(port, "closed")
-        }
+        fmt.Println(port, "closed")
+        time.Sleep(interval)
+        ScanPort(ip, port, timeout - intervall)
         return false
     }
-    
+
     conn.Close()
     fmt.Println(port, "open")
     return true
