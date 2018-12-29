a86542ffc724fcde7d6b0f917760975fc0318aea
cws <cws@home.fritz.box>
2018-12-29 01:59:40 +0100

    testing

--- cmd/create.go
+++ cmd/create.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"time"
+	"log"
 
 	"github.com/spf13/cobra"
 	"github.com/styxlab/kubeprov/pkg/hetzner"
@@ -22,22 +23,17 @@ var createCmd = &cobra.Command{
 func CreateCluster(cmd *cobra.Command, args []string) {
 	
 	hc := hetzner.Connect()
-
 	serverSpec := hc.ServerSpec("cws@home", "demo", "cx11", "centos-7")
-
 	serverInst := serverSpec.Create().EnableRescue().PowerOn().WaitForRunning()
-
+	//serverInst := serverSpec.Status()
 	fmt.Printf("Created node '%s' with IP %s\n", serverInst.Name(), serverInst.IPv4())
 
 	installCoreOS(serverInst.IPv4())
-
-	//serverInst := serverSpec.Status()
-
 	serverInst.Reboot()
+
 	if !ssh.ScanPort(serverInst.IPv4(), 22, 2 * time.Second, 120 * time.Second) {
 		log.Fatal("Portscan failed after timeout.")
 	}
-
 
 	auth := ssh.AuthKey("cws@home", "/home/cws/.ssh/id_ed25519")
 	config := auth.Config("core")
